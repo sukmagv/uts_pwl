68dc2730a4412d252a42718dac69b22d797a5f5c
Yisha Zukhrufin Angelyna <yishaangeli342@gmail.com>
2023-04-13 10:32:53 +0700

    membuat_register

--- database/migrations/2023_04_12_013952_username.php
+++ database/migrations/2023_04_12_013952_username.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function(Blueprint $table){
-            $table->string('username', 10)->after('id')->nullable()->unique()->index();
+            $table->string('username', 10)->after('id')->nullable()->unique()->index()->after('id');
         });
     }
 
